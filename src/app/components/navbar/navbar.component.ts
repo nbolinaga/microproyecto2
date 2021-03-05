da39b7e6f418085f1c50225e793e426ef88a7774
nbolinaga <nbolinaga17@gmail.com>
2021-03-05 15:22:00 -0400

    agregado navbar con login

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+import { Observable } from 'rxjs';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  user: firebase.User = null;
+  logeado: boolean = false;
+  showLogIn: boolean = false;
 
-  ngOnInit(): void {
+  constructor(public authService: LoginService) {
+    this.authService.getCurrentUser().subscribe(user => {
+      this.user = user;
+    })
   }
 
+  ngOnInit(): void {
+
+  }
+  logIn() {
+    this.showLogIn = !this.showLogIn;
+  }
+
+  logOut(){
+    this.authService.logout();
+  }
 }
