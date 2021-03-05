cfa895cac3dc28aa2b6fc6f7412833387c05e2c8
nbolinaga <nbolinaga17@gmail.com>
2021-03-05 11:26:03 -0400

    agregado componente login (de notion)

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private authService: LoginService,
+    private router: Router)
+  { }
+
+  ngOnInit(): void {
+  }
+}
