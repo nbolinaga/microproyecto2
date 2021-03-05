f6cf403134c2c44bfceeb010a5469c94cd92ef95
nbolinaga <nbolinaga17@gmail.com>
2021-03-05 15:20:32 -0400

    agregado config al enviroment de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAzfbKPMqqGJXyB6xfgJ-Si7mcs2j_PYrI",
+    authDomain: "miniproyecto2-8d9c7.firebaseapp.com",
+    projectId: "miniproyecto2-8d9c7",
+    storageBucket: "miniproyecto2-8d9c7.appspot.com",
+    messagingSenderId: "9402490895",
+    appId: "1:9402490895:web:8432d638a834275b1c037a"
+  }
 };
