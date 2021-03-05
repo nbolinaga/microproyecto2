721509a0d998f51a6c4f614badae37f880998d3f
nbolinaga <nbolinaga17@gmail.com>
2021-03-05 15:21:23 -0400

    terminada authentication

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -10,42 +9,21 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-  authForm: FormGroup;
   @Output() sendFormEvent = new EventEmitter();
   @Input() isRegister: boolean = false;
 
   constructor(
-    private fb: FormBuilder,
     private authService: LoginService,
     private router: Router
 
   ) { }
 
   ngOnInit(): void {
-    this.createForm();
   }
-
-  createForm(): void {
-    this.authForm = this.fb.group({
-      displayName: '',
-      email: '',
-      password: '',
-    });
-  }
-
   async googleLogin(){
     const user = await this.authService.loginGoogle();
     if (user) {
-      this.router.navigate(['/perfil']);
+      this.router.navigate(['/']);
     }
-  }
-
-  async onSubmit(){
-    const formValues = {
-      displayName: this.authForm.get('displayName').value,
-      email: this.authForm.get('email').value,
-      password: this.authForm.get('password').value,
-    };
-    this.sendFormEvent.emit(formValues);
   }
 }
