7f416e6c6a7516bdc622d37e25e3f1cfa363d11e
nbolinaga <nbolinaga17@gmail.com>
2021-03-06 00:10:00 -0400

    agregado get peliculas por search

--- src/app/services/peliculas-api.service.ts
+++ src/app/services/peliculas-api.service.ts
@@ -7,10 +7,32 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class PeliculasApiService {
-  URL_API = `https://api.themoviedb.org/3/discover/movie?api_key=5ba4b657ce8be5d3b83bdfd09f8df740&page=1-5`;
+  page: number = 2;
+  KEY = '5ba4b657ce8be5d3b83bdfd09f8df740';
+  URL_ESTRENOS = `https://api.themoviedb.org/3/movie/upcoming?api_key=${this.KEY}`;
+
   constructor(private http: HttpClient) { }
 
   getPeliculas () : Observable<any> {
-    return this.http.get<any>(this.URL_API).pipe(map((data: any) => data.results))
+    return this.http.get<any>(`https://api.themoviedb.org/3/movie/top_rated?api_key=${this.KEY}&language=en-US&page=${this.page}&include_adult=true`).pipe(map((data: any) => data.results))
+  }
+  getEstrenos () : Observable<any> {
+    return this.http.get<any>(this.URL_ESTRENOS).pipe(map((data: any) => data.results))
+  }
+  getBusqueda(search: string) : Observable<any>{
+    return this.http.get<any>(`https://api.themoviedb.org/3/search/movie?api_key=${this.KEY}&language=en-US&query=${search}&page=${this.page}&include_adult=true`).pipe(map((data: any) => data.results))
+  }
+
+  resetPages(){
+    this.page = 1;
+  }
+  nextPage(){
+    this.page++;
+  }
+
+  prevPage(){
+    if(this.page != 1){
+      this.page--;
+    }
   }
 }
