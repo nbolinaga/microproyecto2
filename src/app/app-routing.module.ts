b2e93dba01c028ebc0b43532ba1b8c82724b0c51
nbolinaga <nbolinaga17@gmail.com>
2021-03-06 05:45:48 -0400

    agregado routing a fav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetallesComponent } from './components/detalles/detalles.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ListaComponent } from './components/lista/lista.component';
 import { AuthenticationGuardGuard } from './guards/authentication-guard.guard'
@@ -8,7 +9,8 @@ import { AuthenticationGuardGuard } from './guards/authentication-guard.guard'
 const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'lista', component: ListaComponent},
-  {path: 'detalles/:id', component: DetallesComponent, canActivate: [AuthenticationGuardGuard]}
+  {path: 'detalles/:id', component: DetallesComponent},
+  {path: 'favoritos', component: FavoritesComponent, canActivate: [AuthenticationGuardGuard]}
 ];
 
 @NgModule({
