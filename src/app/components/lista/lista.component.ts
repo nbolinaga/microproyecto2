5de76f600998b18f5ee7820a26ac0aa26d83fa31
nbolinaga <nbolinaga17@gmail.com>
2021-03-06 00:09:03 -0400

    terminada lista

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PeliculasApiService } from '../../services/peliculas-api.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-lista',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private peliculasSrv: PeliculasApiService) { }
+  peliculas : Observable<any>;
+  search: string = "";
+  currentPage: number = 1;
 
   ngOnInit(): void {
+    this.getPeliculas();
   }
 
+  getPeliculas(){
+    this.peliculas = this.peliculasSrv.getPeliculas();
+  }
+
+  getBusqueda(){
+    this.currentPage = 1;
+    this.peliculasSrv.resetPages();
+    this.peliculas = this.peliculasSrv.getBusqueda(this.search);
+    console.log(this.peliculas);
+  }
+
+  nextPage(){
+    this.peliculasSrv.nextPage();
+    this.call();
+    this.currentPage++;
+  }
+
+  prevPage(){
+    this.peliculasSrv.prevPage();
+    this.call();
+    if(this.currentPage != 1)
+    {
+      this.currentPage--;
+    }
+  }
+
+  call(){
+    if(this.search != ""){
+      this.peliculas = this.peliculasSrv.getBusqueda(this.search);
+    } else {
+      this.peliculas = this.peliculasSrv.getPeliculas();
+    }
+  }
 }
